7efec3155f67c4cf5995d288e9232cdc2293ca9a
Ondřej Sluka <ondras12345@gmail.com>
2024-10-17 11:35:01 +0200

    Add support for MySQL / mariadb

--- README.md
+++ README.md
@@ -1,8 +1,58 @@
 # mpay
+TODO description
+
+## Features
 TODO
 
+
 ## Installation
-TODO
+It is recommended to install via [`pipx`](https://github.com/pypa/pipx):
+```sh
+pipx install git+https://github.com/ondras12345/mpay.git
+```
+
+### Setup
+You need to create a config file in `$XDG_CONFIG_HOME/mpay/config.yaml`
+on GNU/Linux, or `%APPDATA$\mpay\config.yaml` on Windows:
+```yaml
+user: your_username  # optional, will use $USER if omitted
+db_url: "sqlite:////home/user/path/to/mpay.db"
+```
+
+Alternatively, you can use a MySQL / mariadb database:
+```yaml
+db_url: "mysql+pymysql://user:password@host/database"
+```
+
+If you don't want to store your database credentials in a text config file,
+you can also specify a command to retrieve it from a password manager:
+```yaml
+db_url:
+  # secret-tool store --label="mpay_test" mpay-db mpay_test
+  command: secret-tool lookup mpay-db mpay_test
+```
+
+This project uses SQLAlchemy to access the database, so other RDBMSs might work
+too, but I haven't tested them.
+It is a good idea to at least run the tests located in `tests/` directory
+against your database.
+
+Create the database tables:
+```sh
+mpay admin init
+```
+
+To execute standing orders, it is recommended to set up your cron to run the
+following command periodically:
+```
+mpay admin cron
+```
+
+You can also use cron to run consistency checks:
+```
+# exit status will be non-zero on failure
+mpay admin check
+```
 
 
 ## Development
--- mpay/db.py
+++ mpay/db.py
@@ -3,11 +3,11 @@ import logging
 import sqlalchemy as sqa
 from sqlalchemy import (
     create_engine, ForeignKey, PrimaryKeyConstraint, CheckConstraint,
-    UniqueConstraint
+    UniqueConstraint, String
 )
 
 from sqlalchemy.orm import (
-    declarative_base, Mapped, mapped_column, relationship
+    DeclarativeBase, Mapped, mapped_column, relationship
 )
 
 from sqlalchemy.orm import Session  # noqa: F401
@@ -22,7 +22,16 @@ from decimal import Decimal
 _LOGGER = logging.getLogger(__name__)
 
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    # extra mysql args: don't forget to include in __table_args__ in derived
+    # classes
+    _mysql_args = {
+        "mysql_default_charset": "utf8mb4",
+        "mysql_collate": "utf8mb4_unicode_520_ci",
+    }
+
+    __table_args__ = _mysql_args
+
 
 money_type = Numeric(precision=9, scale=3, asdecimal=True)
 
@@ -30,13 +39,11 @@ money_type = Numeric(precision=9, scale=3, asdecimal=True)
 def aware_utcnow():
     return datetime.datetime.now(datetime.timezone.utc)
 
-# TODO MySQL collate (utf8 case insensitive ??)
-
 
 class User(Base):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(unique=True)
+    name: Mapped[str] = mapped_column(String(32), unique=True)
     balance: Mapped[Decimal] = mapped_column(money_type)
     # This could use a deferred constraint checking that SUM(balance) = 0,
     # but I don't think MySQL supports that.
@@ -45,44 +52,45 @@ class User(Base):
 class Currency(Base):
     __tablename__ = "currencies"
     id: Mapped[int] = mapped_column(primary_key=True)
-    iso_4217: Mapped[str] = mapped_column(unique=True)
-    name: Mapped[Optional[str]]
+    iso_4217: Mapped[str] = mapped_column(String(3), unique=True)
+    name: Mapped[Optional[str]] = mapped_column(String(32))
 
 
 class Tag(Base):
     __tablename__ = "tags"
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(unique=True)
-    description: Mapped[Optional[str]]
+    name: Mapped[str] = mapped_column(String(32), unique=True)
+    description: Mapped[Optional[str]] = mapped_column(String(255))
     transactions: Mapped[list["Transaction"]] = relationship(secondary="transactions_tags", back_populates="tags")
-    # tags can be a tree
     parent_id: Mapped[Optional[int]] = mapped_column(ForeignKey(__tablename__ + ".id"))
     parent: Mapped[Optional["Tag"]] = relationship(back_populates="children", remote_side=[id])
     children: Mapped[list["Tag"]] = relationship(back_populates="parent")
-    __table_args__ = (
-        CheckConstraint("parent_id <> id"),
-    )
+    # TODO MySQL does not like CHECK on AUTO_INCREMENT column
+    # __table_args__ = (
+    #     CheckConstraint("parent_id <> id", "parent_not_self"),
+    #     Base._mysql_args
+    # )
 
 
 class Agent(Base):
     __tablename__ = "agents"
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(unique=True)
-    description: Mapped[Optional[str]]
+    name: Mapped[str] = mapped_column(String(32), unique=True)
+    description: Mapped[Optional[str]] = mapped_column(String(255))
 
 
 class StandingOrder(Base):
     __tablename__ = "standing_orders"
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str]
+    name: Mapped[str] = mapped_column(String(32))
     user_from_id: Mapped[int] = mapped_column(ForeignKey(User.__tablename__ + ".id"))
     user_from: Mapped[User] = relationship(foreign_keys=[user_from_id])
     user_to_id: Mapped[int] = mapped_column(ForeignKey(User.__tablename__ + ".id"))
     user_to: Mapped[User] = relationship(foreign_keys=[user_to_id])
     amount: Mapped[Decimal] = mapped_column(money_type)
-    note: Mapped[Optional[str]]
+    note: Mapped[Optional[str]] = mapped_column(String(255))
     # rrule dtstart is stored as a naive UTC datetime
-    rrule_str: Mapped[str]
+    rrule_str: Mapped[str] = mapped_column(String(255))
     # UTC date when next transaction should occur or None for disabled /
     # expired order. Cannot be recovered once set to None.
     dt_next_utc: Mapped[Optional[datetime.datetime]]
@@ -90,6 +98,7 @@ class StandingOrder(Base):
     __table_args__ = (
         UniqueConstraint("name", "user_from_id"),
         CheckConstraint("amount > 0"),
+        Base._mysql_args
     )
 
 
@@ -109,7 +118,7 @@ class Transaction(Base):
     standing_order: Mapped[Optional[StandingOrder]] = relationship()
     agent_id: Mapped[Optional[int]] = mapped_column(ForeignKey(Agent.__tablename__ + ".id"))
     agent: Mapped[Optional[Agent]] = relationship()
-    note: Mapped[Optional[str]]
+    note: Mapped[Optional[str]] = mapped_column(String(255))
     dt_created_utc: Mapped[datetime.datetime] = mapped_column(default=aware_utcnow)
     dt_due_utc: Mapped[datetime.datetime]
     tags: Mapped[list[Tag]] = relationship(secondary="transactions_tags", back_populates="transactions")
@@ -120,6 +129,7 @@ class Transaction(Base):
         # Currently, we don't support adding transactions with future due
         # date.
         CheckConstraint("dt_due_utc < dt_created_utc"),
+        Base._mysql_args
     )
 
 
@@ -127,6 +137,7 @@ class TransactionTag(Base):
     __tablename__ = "transactions_tags"
     __table_args__ = (
         PrimaryKeyConstraint("transaction_id", "tag_id"),
+        Base._mysql_args
     )
     transaction_id: Mapped[int] = mapped_column(ForeignKey(Transaction.__tablename__ + ".id"))
     tag_id: Mapped[int] = mapped_column(ForeignKey(Tag.__tablename__ + ".id"))
